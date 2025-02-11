4885658f7af51c8a7f27f3386bccf126a7dce482
shruti-s0405 <shruti.sundar0405@gmail.com>
2025-02-11 17:30:12 +0530

    Initial commit with frontend and backend

---
+++ quizapp/src/main/java/com/zephyra/quizapp/controller/QuizController.java
@@ -0,0 +1,40 @@
+package com.zephyra.quizapp.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zephyra.quizapp.DTO.QuestionWrapper;
+import com.zephyra.quizapp.Model.Response;
+import com.zephyra.quizapp.service.QuizService;
+
+@RestController
+@RequestMapping("quiz")
+
+public class QuizController {
+    @Autowired
+    QuizService quizService;
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title ){
+        return quizService.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping("get/{Id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer Id){
+        return quizService.getQuizQuestions(Id);
+    }
+
+    @PostMapping("submit/{Id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer Id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(Id, responses);
+    }
+}
---
+++ quizapp/src/main/java/com/zephyra/quizapp/service/QuizService.java
@@ -0,0 +1,59 @@
+package com.zephyra.quizapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import java.util.*;
+import com.zephyra.quizapp.Model.Quiz;
+import com.zephyra.quizapp.Repository.QuestionRepository;
+import com.zephyra.quizapp.DTO.QuestionWrapper;
+import com.zephyra.quizapp.Model.Question;
+import com.zephyra.quizapp.Repository.QuizRepository;
+import com.zephyra.quizapp.Model.Response;
+
+
+@Service
+public class QuizService {
+    @Autowired
+    QuestionRepository questionRepository;
+    @Autowired
+    QuizRepository quizRepository;
+    public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+        
+        List<Question> questions = questionRepository.findRandomQuestionsByCategory(category, numQ);
+        
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setQuestions(questions);
+        quizRepository.save(quiz);
+
+        return new ResponseEntity<>("Successs", HttpStatus.CREATED);
+    }
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer Id) {
+        Optional<Quiz> quiz = quizRepository.findById(Id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+
+        for(Question q : questionsFromDB){
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionText(), q.getOption1(), q.getOption2(), q.getOption3());
+            questionsForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+
+    }
+
+    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses){
+        Quiz quiz = quizRepository.findById(id).get();
+        List<Question> questions = quiz.getQuestions();
+        int right = 0;
+        int i = 0;
+        for(Response response :responses){
+            if(response.getResponse().equals(questions.get(i).getCorrectAnswer()))
+                right++;
+            i++;
+        }
+        return new ResponseEntity<>(right, HttpStatus.OK);
+    }
+}
